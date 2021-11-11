000d890770cc7dfa5a56db331f890acbc3920446
Vito Ka <nathanael.vito@ti.ukdw.ac.id>
2021-11-11 15:00:23 +0700

    Tambah file 3_A_71210681

---
+++ 3_A_71210681.py
@@ -0,0 +1,10 @@
+daftar1 = input("Masukan daftar belanja 1 : ")
+daftar2 = input("Masukan daftar belanja 2 : ")
+tambah_daftar1 = input("Tambahkan data ke daftar belanja 1 : ")
+tambah_daftar2 = input("Tambahkan data ke daftar belanja 2 : ")
+
+daftar_belanja1 = [daftar1, tambah_daftar1]
+daftar_belanja2 = [daftar2, tambah_daftar2]
+
+print("Daftar belanja 1 adalah", daftar_belanja1[0], daftar_belanja1[1])
+print("Daftar belanja 2 adalah", daftar_belanja2[0], daftar_belanja2[1])
