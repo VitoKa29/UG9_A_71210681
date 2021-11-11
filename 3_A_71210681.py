26ba83bc75b4b0456e95dca12eb3cd36f99d290c
Vito Ka <nathanael.vito@ti.ukdw.ac.id>
2021-11-11 15:46:51 +0700

    Tambah file 3_A_71210681

--- 3_A_71210681.py
+++ 3_A_71210681.py
@@ -6,5 +6,5 @@ tambah_daftar2 = input("Tambahkan data ke daftar belanja 2 : ")
 daftar_belanja1 = [daftar1, tambah_daftar1]
 daftar_belanja2 = [daftar2, tambah_daftar2]
 
-print("Daftar belanja 1 adalah", daftar_belanja1[0], daftar_belanja1[1])
-print("Daftar belanja 2 adalah", daftar_belanja2[0], daftar_belanja2[1])
+print("Daftar belanja 1 adalah", daftar_belanja1[0],",",daftar_belanja1[1])
+print("Daftar belanja 2 adalah", daftar_belanja2[0],",",daftar_belanja2[1])
