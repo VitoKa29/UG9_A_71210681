9afc4a5c7aa00cd5237a9e94321940d9b50edc2c
Vito Ka <nathanael.vito@ti.ukdw.ac.id>
2021-11-11 14:53:26 +0700

    Hapus File

--- 1_A_71210681.py
+++
@@ -1,3 +0,0 @@
-diameter = int(input("Masukan diameter : "))
-keliling = 3.14 * diameter
-print("Keliling lingkaran adalah : ", round(keliling,2))
--- 2_A_71210681.py
+++
@@ -1,3 +0,0 @@
-angka = int(input("Masukan angka : "))
-perpangkatan = angka ** 5
-print(angka, " pangkat 5 adalah : ", perpangkatan)
